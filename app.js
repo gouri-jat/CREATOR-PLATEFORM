9d68b5dcc217deb21e94e08be5d7eb9633e47464
Gouri Jat <gourijat26@gmail.com>
2026-01-31 23:16:15 +0530

    adding app.js file

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./db");
+connectDB();
+app.use(express.json());
+const authRoutes = require("./routes/authRoutes"); 
+const blogRoutes = require("./routes/blogRoutes");
+const creatorRoutes = require("./routes/creatorRoutes");
+
+app.use("/api/auth",authRoutes);
+app.use("/api/blog",blogRoutes);
+app.use("/api/creator",creatorRoutes);
+
+
+module.exports = app;   
