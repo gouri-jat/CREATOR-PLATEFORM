37ddf9a8f29efb20965c342f29559f65e3d12079
Gouri Jat <gourijat26@gmail.com>
2026-05-05 12:12:13 +0530

    first-commit

--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     },
     isCreator :{
         type:Boolean,
-        default:"false"
+        default:false
     },
      creatorProfile: {
       bio: String,
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema({
       },
     ],
   
-},{timeStamps : true});
+},{timestamps : true});
 
 
 module.exports =  mongoose.model("User",userSchema);
