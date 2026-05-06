258dff3039f3eb0fa6cea80ad29072b92cd95c1e
Gouri Jat <gourijat26@gmail.com>
2026-05-06 23:20:15 +0530

    Adding server file

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const app = require("./app");
+const app = require("./app"); 
+const connectDB = require("./db");
+
 const PORT = process.env.PORT || 5000;
 
+connectDB.then(()=>{
 app.listen(PORT,()=>{
     console.log(`Server started at ${PORT}`);
+});
 });
