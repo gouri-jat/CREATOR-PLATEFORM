320475573fad67e270bfa6e1d83eeda5de6549bb
Gouri Jat <gourijat26@gmail.com>
2026-05-06 23:18:33 +0530

    Adding models

--- models/blog.js
+++ models/blog.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
-    content: { type: String, required: true },
+    title: { type: String, required: true  , trim : true , maxlength : 100},
+    content: { type: String, required: true , trim : true},
     author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true
+      required: true,
+      trim : true,
+      index : true
     }
   },
   { timestamps: true }
--- models/user.js
+++ models/user.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
     name :{
          type : String,
-         required : true
+         required : true,
+         trim : true
     },
     email:{
         type:String,
         required:true,
-        unique:true
+        unique:true,
+        lowercase : true,
+        trim : true
     },
     password :{
         type:String,
         required:true
+
     },
-    roles:{
+    role:{
         type:String, 
         enum:["user","admin"],
         default:"user"
@@ -32,10 +37,17 @@ const userSchema = new mongoose.Schema({
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
+        default:[]
       },
     ],
   
 },{timestamps : true});
+
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 
 module.exports =  mongoose.model("User",userSchema);
