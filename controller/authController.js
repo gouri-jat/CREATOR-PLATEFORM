15563da6b62ac9628a91ae7011a69aa76264e85b
Gouri Jat <gourijat26@gmail.com>
2026-05-06 23:16:43 +0530

    Adding controllers

--- controller/authController.js
+++ controller/authController.js
@@ -5,20 +5,17 @@ const bcrypt = require("bcryptjs");
 exports.registerUser = async(req,res)=>{
     try{
       const {name,email,password} = req.body;
-      const user = await User.create({name, email , password : hashedPassword});
+    
       const existingUser = await User.findOne({email});
      if(existingUser){
             return res.status(400).json({message : "User already exists"});
         }
-        const hashedPassword = await bcrypt.hash(password,10);
+        
         const user = await User.create({
             name,
             email,
             password : hashedPassword,
-            role,
-            isCreator,
-            creatorProfile,
-            followers
+           
         });
         res.status(201).json({
             message : "User registered successfully",
--- controller/blogController.js
+++ controller/blogController.js
@@ -1,13 +1,20 @@
 const Blog = require("../models/blog");
 exports.createBlog = async (req, res) => {
-  try {
+  try { 
+    const {title , content } = req.body;
+    if(!title || !content){
+      return res.status(400).json({ message: "Title and content are required" });
+    }
     const blog = await Blog.create({
       title: req.body.title,
       content: req.body.content,
       author: req.user.id   // VERY IMPORTANT
     });
 
-    res.status(201).json(blog);
+    res.status(201).json({
+      message : "Blog created successfully",
+      blog
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -15,8 +22,15 @@ exports.createBlog = async (req, res) => {
 
 exports.getMyBlogs = async (req, res) => {
   try {
-    const blogs = await Blog.find({ author: req.user.id });
-    res.status(200).json(blogs);
+    const blogs = await Blog.find({ author: req.user.id })
+     .populate("author", "name email")
+     .sort({ createdAt : -1 });
+
+    res.status(200).json({
+      message : "Blogs fetched successfully",
+      blogs
+    });
+    
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
