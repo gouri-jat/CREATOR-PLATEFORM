44c6ae0f315d6f786d09b989510b31599e07c8eb
Gouri Jat <gourijat26@gmail.com>
2026-05-06 23:12:06 +0530

    Adding app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const connectDB = require("./db");
-connectDB();
+
 
 const authRoutes = require("./routes/authRoutes"); 
 const blogRoutes = require("./routes/blogRoutes");
 const creatorRoutes = require("./routes/creatorRoutes");
 
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 app.use("/api/auth",authRoutes);
 app.use("/api/blog",blogRoutes);
 app.use("/api/creator",creatorRoutes);
